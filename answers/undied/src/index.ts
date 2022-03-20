ad63ce23e515e205e16b0ccb05aaafcba10abb00
UndiedGamer <hemanthchakka2007@gmail.com>
2022-03-20 10:40:35 +0530

    Refactor code and fix bugs

--- answers/undied/src/index.ts
+++ answers/undied/src/index.ts
@@ -13,10 +13,8 @@ class LectureManager {
 	public check() {
 		// Step 1: Iterate through the timings and remove one of the conflicting timing.
 		this.iterate();
-		// Step 2: Filter out the remaining timings.
-		this.filter();
-		// Step 3: Return how many lectures one can attend.
-		return new Set(this.messages).size;
+		// Step 2: Return how many lectures one can attend.
+		return new Set(this.left.start).size;
 	}
 
 	public setTimes(start: number[], end: number[]) {
@@ -26,30 +24,19 @@ class LectureManager {
 	}
 
 	private iterate() {
+		this.left = {
+			start: [],
+			end: []
+		}
+		this.messages = [];
 		for (const [i, v] of this.start.entries()) {
 			if (this.end[i] > this.start[i + 1]) {
 				this.main = i;
-				this.pushMessage(this.start[i], this.end[i]);
 			} else {
 				this.left.start.push(v);
 				this.left.end.push(this.end[i]);
 			}
 		}
-	}
-
-	private filter() {
-		const filtered = this.left.start.filter((_v, i, a) => {
-			if (a.length === 1) return this.end[this.main] > a[i];
-			return this.end[this.main] > a[i + 1];
-		});
-		for (const v of filtered) {
-			const i = this.left.start.indexOf(v);
-			return this.pushMessage(this.left.start[i], this.left.end[i]);
-		}
-	}
-
-	private pushMessage(start: number, end: number) {
-		this.messages.push(`${start} - ${end}`);
 	}
 }
 
@@ -60,3 +47,7 @@ console.log(`Can attend ${manager.setTimes([1, 3, 5, 8, 9], [2, 6, 7, 10, 11]).c
 // => Can attend 3 lectures
 console.log(`Can attend ${manager.setTimes([1, 3, 5], [2, 4, 6]).check()} lectures`);
 // => Can attend 3 lectures
+console.log(`Can attend ${manager.setTimes([1, 3, 5, 7], [2, 4, 8, 9]).check()} lectures`);
+// => Can attend 3 lectures
+console.log(`Can attend ${manager.setTimes([1, 3, 5, 7], [2, 4, 6, 8]).check()} lectures`);
+// => Can attend 4 lectures
