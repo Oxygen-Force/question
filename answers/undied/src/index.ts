550e052922b045d6264d814f806b88b92085171f
UndiedGamer <hemanthchakka2007@gmail.com>
2022-03-26 16:11:24 +0530

    Rewrite code for better performance

--- answers/undied/src/index.ts
+++ answers/undied/src/index.ts
@@ -1,51 +1,31 @@
-// Run this file via npm start or yarn start
-
-class LectureManager {
-	public start: number[];
-	public end: number[];
-	public main: number;
-	public left = {
-		start: [] as number[],
-		end: [] as number[]
+// Run npm install or yarn install and then run npm start or yarn start
+function check(start: number[], end: number[]) {
+	/**
+	 * Local variables
+	 */
+	const left = {
+		start: [],
+		end: []
 	};
 
-	public check() {
-		// Step 1: Iterate through the timings and remove one of the conflicting timing.
-		this.iterate();
-		// Step 2: Return how many lectures one can attend.
-		return new Set(this.left.start).size;
-	}
-
-	public setTimes(start: number[], end: number[]) {
-		this.start = start;
-		this.end = end;
-		return this;
-	}
-
-	private iterate() {
-		this.left = {
-			start: [],
-			end: []
-		};
-		for (const [i, v] of this.start.entries()) {
-			if (this.end[i] > this.start[i + 1]) {
-				this.main = i;
-			} else {
-				this.left.start.push(v);
-				this.left.end.push(this.end[i]);
-			}
+	//
+	for (const [i, v] of start.entries()) {
+		const filtered = start.filter((_v, ind) => ind > i);
+		if (!filtered.some((val) => end[i] > val)) {
+			left.start.push(v);
+			left.end.push(end[i]);
 		}
 	}
+	return left.start.length;
 }
 
-const manager = new LectureManager();
-console.log(`Can attend ${manager.setTimes([1, 3, 5], [2, 6, 7]).check()} lectures`);
+console.log(`Can attend ${check([1, 3, 5], [2, 6, 7])} lectures`);
 // => Can attend 2 lectures
-console.log(`Can attend ${manager.setTimes([1, 3, 5, 8, 9], [2, 6, 7, 10, 11]).check()} lectures`);
+console.log(`Can attend ${check([1, 3, 5, 8, 9], [2, 6, 7, 10, 11])} lectures`);
 // => Can attend 3 lectures
-console.log(`Can attend ${manager.setTimes([1, 3, 5], [2, 4, 6]).check()} lectures`);
+console.log(`Can attend ${check([1, 3, 5], [2, 4, 6])} lectures`);
 // => Can attend 3 lectures
-console.log(`Can attend ${manager.setTimes([1, 3, 5, 7], [2, 4, 8, 9]).check()} lectures`);
+console.log(`Can attend ${check([1, 3, 5, 7], [2, 4, 8, 9])} lectures`);
 // => Can attend 3 lectures
-console.log(`Can attend ${manager.setTimes([1, 3, 5, 7], [2, 4, 6, 8]).check()} lectures`);
+console.log(`Can attend ${check([1, 3, 5, 7], [2, 4, 6, 8])} lectures`);
 // => Can attend 4 lectures
